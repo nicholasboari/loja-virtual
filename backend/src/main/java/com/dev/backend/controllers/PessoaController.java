8cf6c8b053b1a1dd66de46ba3d0ecdc7b69d261a
nicholasboari <nicholasboari@gmail.com>
2022-12-16 21:03:30 -0300

    crud pessoa

---
+++ backend/src/main/java/com/dev/backend/controllers/PessoaController.java
@@ -0,0 +1,51 @@
+package com.dev.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.backend.entities.Pessoa;
+import com.dev.backend.services.PessoaService;
+
+@RestController
+@RequestMapping("/api/pessoa")
+public class PessoaController {
+
+  @Autowired
+  private PessoaService pessoaService;
+
+  @GetMapping("/{id}")
+  public Pessoa buscarPorId(@PathVariable("id") Long id) {
+    return pessoaService.buscarPorId(id);
+  }
+
+  @GetMapping("/")
+  public List<Pessoa> buscarTodos() {
+    return pessoaService.buscarTodos();
+  }
+
+  @PostMapping("/")
+  public Pessoa inserir(@RequestBody Pessoa obj) {
+    return pessoaService.inserir(obj);
+  }
+
+  @PutMapping("/")
+  public Pessoa alterar(@RequestBody Pessoa obj) {
+    return pessoaService.alterar(obj);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
+    pessoaService.excluir(id);
+    return ResponseEntity.ok().build();
+  }
+}
