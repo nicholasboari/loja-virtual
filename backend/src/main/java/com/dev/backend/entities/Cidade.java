0aab5e92507c00548237173e41bf5645187080df
nicholasboari <nicholasboari@gmail.com>
2022-12-19 16:55:12 -0300

    fix cidade column

--- backend/src/main/java/com/dev/backend/entities/Cidade.java
+++ backend/src/main/java/com/dev/backend/entities/Cidade.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -23,6 +24,7 @@ public class Cidade {
   private String nome;
 
   @ManyToOne()
+  @JoinColumn(name = "estado_id")
   private Estado estado;
 
   @Temporal(TemporalType.TIMESTAMP)
